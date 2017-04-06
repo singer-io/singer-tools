5bd6a9b17e940c0748abf6d6647f31bdc4721f83
Mike DeLaurentis <miked@stitchdata.com>
2017-04-06 12:07:42 -0400

    0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='singer-tools',
-      version='0.0.2',
+      version='0.1.0',
       description='Tools for working with Singer.io Taps and Targets',
       author='Stitch',
       url='http://singer.io',
