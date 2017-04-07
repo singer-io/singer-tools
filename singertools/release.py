451987abc89f2e57f2e2adf9fe2c178cbd0efad8
Mike DeLaurentis <miked@stitchdata.com>
2017-04-07 09:58:24 -0400

    Working on release

--- singertools/release.py
+++ singertools/release.py
@@ -1,4 +1,5 @@
 import subprocess
+import re
 
 def git(*args):
     cmd = ['git'] + list(args)
@@ -32,9 +33,28 @@ def git_check_status():
 def git_push():
     git('push')
 
+def find_version_number():
+    candidates = []
+    with open('setup.py') as setup:
+        for line in setup:
+            m = re.match(r'^\s+version=(.*),', line)
+            if m:
+                candidates.append(m.group(1))
+    if len(candidates) == 1:
+        return candidates[0]
+    elif len(candidates) == 0:
+        print("I couldn't find the version number in setup.py. Please make sure there's a line that looks like 'version=x.x.x,'.")
+        exit(1)
+    else:
+        print("I found multiple candidates for the version number in setup.py. Please make sure there's exactly one line that looks like 'version=x.x.x,'.")
+        exit(1)        
+    
 def main():
+    version = find_version_number()
+    print(version)
     git_check_branch()
     git_check_status()
-    git_push()
+    git('push')
+    git('tag', '-a', 'v'+version, '-m', 'version '+version)
+    git('push', '--tags')
     subprocess.call(['python', 'setup.py', 'sdist', 'upload'])
-
