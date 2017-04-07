bd9c84d9c4289e34c0de50716a4636a98b5e7caa
Mike DeLaurentis <miked@stitchdata.com>
2017-04-07 10:00:48 -0400

    Find version number correctly

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='singer-tools',
-      version='0.1.0',
+      version='0.2.0',
       description='Tools for working with Singer.io Taps and Targets',
       author='Stitch',
       url='http://singer.io',
