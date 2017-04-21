6c3d80d9b3775898ad0b3de5ea66406426c62d15
Mike DeLaurentis <miked@stitchdata.com>
2017-04-21 13:08:57 -0400

    Pylint

--- singertools/release.py
+++ singertools/release.py
@@ -19,7 +19,7 @@ def fail(msg):
 
 def git_check_branch():
     branch_lines = git('rev-parse', '--abbrev-ref', 'HEAD')
-    if len(branch_lines) == 0:
+    if not branch_lines:
         fail('Could not determine branch')
     branch = branch_lines[0]
     if branch != 'master':
@@ -27,7 +27,7 @@ def git_check_branch():
 
 def git_check_status():
     status_lines = git('status', '--porcelain')
-    if len(status_lines) > 0:
+    if status_lines:
         fail('You have uncommitted changes')
 
 def git_push():
@@ -37,18 +37,22 @@ def find_version_number():
     candidates = []
     with open('setup.py') as setup:
         for line in setup:
-            m = re.match(r'^\s+version=\'(\d\.\d\.\d)\',', line)
-            if m:
-                candidates.append(m.group(1))
+            match = re.match(r'^\s+version=\'(\d\.\d\.\d)\',', line)
+            if match:
+                candidates.append(match.group(1))
     if len(candidates) == 1:
         return candidates[0]
-    elif len(candidates) == 0:
-        print("I couldn't find the version number in setup.py. Please make sure there's a line that looks like 'version='x.x.x','.")
+    elif not candidates:
+        print("I couldn't find the version number in setup.py. " +
+              "Please make sure there's a line that looks like " +
+              "'version='x.x.x','.")
         exit(1)
     else:
-        print("I found multiple candidates for the version number in setup.py. Please make sure there's exactly one line that looks like 'version=x.x.x,'.")
-        exit(1)        
-    
+        print("I found multiple candidates for the version number in " +
+              "setup.py. Please make sure there's exactly one line that " +
+              "looks like 'version=x.x.x,'.")
+        exit(1)
+
 def main():
     version = find_version_number()
     git_check_branch()
